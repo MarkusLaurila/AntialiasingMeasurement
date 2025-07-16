3e3d7cf70f38e0436ab2e9310325472dd4721cec
Make <markuslaurila@kamk.fi>
2025-07-16 09:50:34 +0300

    IMGUI added with its own CMakeList ImageCapture.cpp and header added

---
+++ header/ImageCapture.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef IMAGECAPTURE_H
+#define IMAGECAPTURE_H
+#include <glew.h>
+#include <GLFW/glfw3.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <inc/SOIL/SOIL.h>
+
+void saveScreenShot(int width, int height, const char *filename);
+
+
+
+
+
+
+#endif //IMAGECAPTURE_H
